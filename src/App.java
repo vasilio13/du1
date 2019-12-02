67d3b8b69ad374de63a837d9cf194ed99f96474c
vasilio13 <vasilkhaliaukin@gmail.com>
2019-12-02 21:28:39 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(5 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("/0");
+        } finally {
+            System.out.println(10/2);
+        }
+        System.out.println("life is life");
+
+    }
+}
+
